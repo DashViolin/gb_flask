b5f2d2c6a7c5b324768d55921dac335915cf978c
DashViolin <dasha_erminski@mail.ru>
2023-08-05 17:59:09 +0300

    lesson02 - simple blog app with 2 apps, templates and static

--- app/blog/app.py
+++ app/blog/app.py
@@ -1,36 +1,26 @@
-from time import time
+from flask import Flask
+from template_filters import rus_datetime_fmt
 
-from flask import Flask, Response, g, request
-
-app = Flask(__name__)
-
-
-@app.route("/<string:parameter>", methods=["GET", "POST"])
-def index(parameter: str):
-    query_string = f"<br/>Query: {request.query_string.decode('utf8')}" if request.query_string else ""
-    content = f"<p>Hello!<br/>Param: {parameter}{query_string}<p>"
-    return Response(f"<html><head></head><body>{content}</body></html>")
+from .articles.views import article
+from .index.views import index
+from .users.views import user
 
 
-@app.before_request
-def before_request():
-    g.start_time = time()
+def register_blueprints(app: Flask):
+    app.register_blueprint(index)
+    app.register_blueprint(user)
+    app.register_blueprint(article)
 
 
-@app.after_request
-def after_request(response: Response):
-    if hasattr(g, "start_time"):
-        response.headers["X-Process-Time"] = time() - g.start_time
-    return response
+def register_template_filters(app: Flask):
+    app.add_template_filter(rus_datetime_fmt)
 
 
-@app.errorhandler(404)
-def handler_404(error_msg):
-    app.logger.error(error_msg)
-    return "Not found!", 404
-
-
-@app.errorhandler(ZeroDivisionError)
-def handle_zero_division_error(error):
-    app.logger.exception(error)
-    return "Error!", 500
+def create_app() -> Flask:
+    """
+    Flask app fabric pattern
+    """
+    app = Flask(__name__)
+    register_blueprints(app)
+    register_template_filters(app)
+    return app
---
+++ app/blog/db/data_mock.py
@@ -0,0 +1,26 @@
+import datetime
+import json
+import pathlib
+
+# test data created with https://www.databasetestdata.com
+
+current_path = pathlib.Path().resolve()
+
+users_fixture = current_path / r"app/blog/db/fixtures/users.json"
+articles_fixture = current_path / r"app/blog/db/fixtures/articles.json"
+
+USERS = {}
+ARTICLES = {}
+
+
+def convert_datetime(datetime_str: str):
+    return datetime.datetime.fromisoformat(datetime_str.replace("Z", "+00:00")).replace(microsecond=0)
+
+
+for data_dict, filename in [(USERS, users_fixture), (ARTICLES, articles_fixture)]:
+    with open(filename) as file:
+        data_list = json.load(file)
+        for data_obj in data_list:
+            data_dict[data_obj["id"]] = data_obj
+            created_at = data_dict[data_obj["id"]]["created_at"]
+            data_dict[data_obj["id"]]["created_at"] = convert_datetime(created_at)
